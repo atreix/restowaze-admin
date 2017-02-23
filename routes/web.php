98b36a955ad4f96ca1da64706e2737010519679c
atreix <praetoriux@gmail.com>
2017-02-24 07:03:50 +0800

    Terms of Service / Privacy Policy

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,12 @@ Route::get('/', 'MainController@index');
 Route::get('/home', 'MainController@index')->name('restowaze-path');
 Route::get('/detail/{id}', 'MainController@showDetails');
 Route::post('/detail/{id}', 'MainController@saveReview')->name('write-review');
+Route::get('/terms-of-service', function () {
+    return View::make('terms');
+});
+Route::get('/privacy-policy', function () {
+    return View::make('privacy');
+});
 
 # Sign in
 Route::get('/signin', 'SignInController@index')->name('sign-in');
